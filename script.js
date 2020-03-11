c765dd1df89fe0af181ac55db69da54aa41a0755
vladislav t <vt118@mail.ru>
2020-03-12 00:44:00 +0300

    feat: add background color change on slider

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const navMenu = document.getElementById('menu');
     const menuItems = document.querySelectorAll('li a');
 
+    const slider = document.getElementById('slider');
+    
+
     const prev = document.getElementById('prev');
     const next = document.getElementById('next');
     const slides = document.querySelectorAll('.slide');
@@ -19,6 +22,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     
     function nextSlide() {
+        let sliderStyles = window.getComputedStyle(slider);
+        sliderStyles.backgroundColor === 'rgb(240, 108, 100)' ? slider.style.backgroundColor = 'rgb(100, 139, 240)' : slider.style.backgroundColor = 'rgb(240, 108, 100)';
         index++;
         if(index === totalSlides) {index = 0;}
 
@@ -26,10 +31,14 @@ document.addEventListener('DOMContentLoaded', () => {
             slides[i].classList.remove('slide--active');
         }
         slides[index].classList.add('slide--active');
+
+        
     }
 
     function prevSlide() {
-        
+        let sliderStyles = window.getComputedStyle(slider);
+        sliderStyles.backgroundColor === 'rgb(240, 108, 100)' ? slider.style.backgroundColor = 'rgb(100, 139, 240)' : slider.style.backgroundColor = 'rgb(240, 108, 100)';
+
         if(index === 0) {index = totalSlides;}
         index--;
         for(let i = 0; i < totalSlides; i++) {
