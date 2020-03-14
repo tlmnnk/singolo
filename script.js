2c1b899ddcf95515ec6d5a823c45c65b6a60a8a1
Vladislav T <vt118@mail.ru>
2020-03-14 19:01:55 +0300

    feat: add portfolio items tag sorting onclick

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+    //DOMContentLoaded - when dom loaded
+    //window.onload - when all loaded including styles
     const navMenu = document.getElementById('menu');
     const menuItems = document.querySelectorAll('li a');
 
@@ -34,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function portfolioItemBorder(e) {
         
         const targetClasses = e.target.parentNode.classList;
-        console.log(targetClasses.contains('portfolio__item--border'));
+        console.log(e);
         if(targetClasses.contains('portfolio__item--border')) {
             targetClasses.remove('portfolio__item--border');
         } else {
@@ -87,4 +89,52 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         slides[index].classList.add('slide--active');
     }
-});
+
+    addTagsClickHandler();
+   
+}); 
+
+const addTagsClickHandler = () => {
+    document.querySelector('.portfolio__tags').addEventListener('click', (e) => {
+        if(e.target.classList.contains('tag')) {
+            let target = e.target;
+            //console.log(e);
+            removeSelectedTags();
+            selectClickedTag(target);
+            if(target.innerText === 'All') {
+                showAllPortfolioItems();
+            } else {
+                filterPortfolioItemsBySelectedTag(target.innerText);
+            }
+        }
+    });
+};
+
+const removeSelectedTags = (target) => {
+    let tags = document.querySelectorAll('.portfolio__tags .tag');
+    tags.forEach(tag => {
+        tag.classList.remove('tag--selected');  
+    });
+    
+};
+
+const selectClickedTag = (clickedTag) => {
+    clickedTag.classList.add('tag--selected');
+};
+
+const showAllPortfolioItems = () => {
+    let portfolioItems = document.querySelectorAll('.portfolio__layout .portfolio__item');
+    portfolioItems.forEach(portfolioItem => {
+        portfolioItem.classList.remove('portfolio__item--hidden');
+    });
+};
+
+const filterPortfolioItemsBySelectedTag = (selectedTag) => {
+    let portfolioItems = document.querySelectorAll('.portfolio__layout .portfolio__item');
+    portfolioItems.forEach(portfolioItem => {
+        portfolioItem.classList.add('portfolio__item--hidden');
+        if(portfolioItem.dataset.tags.includes(selectedTag)) {
+            portfolioItem.classList.remove('portfolio__item--hidden');
+        }
+    });
+};
